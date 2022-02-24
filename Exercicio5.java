38523d94e65da68da68dc728bf4eb8d29abf1087
Yago Siqueira <yagosiqti@gmail.com>
2022-02-24 18:09:56 -0300

    Realizacao do exercicio 5

---
+++ Exercicio5.java
@@ -0,0 +1,25 @@
+//5. Ler as dimensões de um retângulo (base e altura), calcular e escrever a área do
+//retângulo.
+
+import javax.swing.JOptionPane; 
+public class Exercicio5 {
+    public static void main(String[] args) {
+        double b, a, res;
+
+        b = Double.parseDouble(JOptionPane.showInputDialog(
+            "Informe o valor da base do retangulo"
+        ));
+
+        a = b = Double.parseDouble(JOptionPane.showInputDialog(
+            "Informe o valor da altura do retangulo"
+        ));
+
+        res = b * a;
+
+        JOptionPane.showMessageDialog(null,
+        String.format(
+            "A area do retangulo = %.2f",
+            res
+        ));
+    }
+}
